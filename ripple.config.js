3e549474fc0bf62fa64c5fff8a8d18f87eb74600
rokybeast <sajid.shaik116@gmail.com>
2025-07-06 17:27:33 +0530

    Added color gen, along w/ `tinycolor2`.

--- ripple.config.js
+++ ripple.config.js
@@ -1,2 +1,33 @@
-// RippleKit's Config, use this, to make custom *.css
-module.exports = {};
+// RippleKit's Config, use this, to make custom SCSS -> CSS.
+
+module.exports = {
+    palette: {
+        blue: "#0d6efd",
+        gray: "#6c757d",
+        yellow: "#ffc107",
+        red: "#dc3545",
+        green: "#198754",
+        orange: "#fd7e14",
+        purple: "#6f42c1",
+        pink: "#d63384",
+        teal: "#20c997",
+        indigo: "#6610f2",
+        cyan: "#0dcaf0",
+        white: "#ffffff",
+        black: "#000000",
+        lightGray: "#f8f9fa",
+        darkGray: "#343a40",
+    },
+    theme: {
+        colors: {
+            primary: "blue",
+            secondary: "gray",
+            warning: "yellow",
+            danger: "red",
+            success: "green",
+            information: "cyan",
+            white: "white",
+            black: "black",
+        },
+    },
+};
